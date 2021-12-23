67eb2c22353faed944e0e513c142b38ef40184e4
Aslak Johansen <aslakj@gmail.com>
2021-12-23 11:14:19 +0100

    Added a separate function to work as a channel consumer

--- dde-mdds-ingress/ingress.go
+++ dde-mdds-ingress/ingress.go
@@ -18,11 +18,20 @@ type Message struct {
     DeviceID     string  `json:"DeviceID"`
 }
 
-func insert (deviceid string, sensorid string, timestamp string, value float64) {
-  
+func insert (ch chan Message) {
+  for msg := range ch {
+    var deviceid  string  = msg.DeviceID
+    var sensorid  string  = msg.SensorID
+    var timestamp string  = msg.TimeStamp
+    var value     float64 = msg.Value
+    fmt.Println(deviceid, sensorid, timestamp, value)
+  }
 }
 
 func main () {
+  var ch chan Message = make(chan Message, 16)
+  for i:=0; i<1; i++ { go insert(ch) }
+  
   c, err := kafka.NewConsumer(&kafka.ConfigMap{
     "bootstrap.servers": "localhost",
     "group.id":          "myGroup",
@@ -41,12 +50,7 @@ func main () {
       var msg Message
       err := json.Unmarshal(encoded.Value, &msg)
       if err == nil {
-        var deviceid  string  = msg.DeviceID
-        var sensorid  string  = msg.SensorID
-        var timestamp string  = msg.TimeStamp
-        var value     float64 = msg.Value
-        fmt.Println(deviceid, sensorid, timestamp, value)
-        insert(deviceid, sensorid, timestamp, value)
+        ch <- msg
       }
     } else {
       // The client will automatically try to recover from all errors.
